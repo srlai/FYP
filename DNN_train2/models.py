52b16aa02e5ab61f2f7bf6c00bc362ff0b7e7481
jasontsang221 <jasontsang221@gmail.com>
2018-10-12 11:09:35 +0800

    Update DNN_train2/models.py

--- DNN_train2/models.py
+++ DNN_train2/models.py
@@ -48,10 +48,14 @@ class FullChannels(nn.Module):
         self.height = int(256)
         self.header = torch.IntTensor([0,0,0,0])
         self.seen = 0
-        def conv_bn(inp, oup, stride):
+        def newlayer(inp, mid, oup, stride):
             return nn.Sequential(
-                nn.Conv2d(inp, oup, 3, stride, 1, bias=True),
-                nn.ReLU(inplace=True)
+                nn.Conv2d(inp, mid, 1, 1, 0, bias = False) #conv2_1/expand
+                nn.ReLU6(inplace = True)
+                nn.Conv2d(mid, mid, 3, stride, 1, bias = False) #conv2_1/depthwise
+                nn.ReLU6(inplace = True)
+                nn.Conv2d(mid, oup, 1, 1, 0, bias = False) #conv2_1/linear
+                nn.ReLU6(inplace = True) #need ammend
             )
         def conv_dw(inp, oup, stride):
             return nn.Sequential(
@@ -63,34 +67,27 @@ class FullChannels(nn.Module):
         self.model = nn.Sequential(
             nn.Conv2d(3, 32, 3, 2, 1, bias = False) #conv1
             nn.ReLU6(inplace = True)
-            nn.Conv2d(32, 32, 1, 1, 0, bias = False) #conv2_1/expand
+            newlayer(32, 32, 16, 1) #conv2_1
+            newlayer(16, 96, 24, 2) #conv2_2
+            newlayer(24, 144, 24, 1) #conv3_1
+            newlayer(24, 144, 32, 2) #conv3_2
+            newlayer(32, 192, 32, 1) #conv4_1
+            newlayer(32, 192, 32, 1) #conv4_2
+            newlayer(32, 192, 64, 1) #conv4_3
+            newlayer(64, 384, 64, 1) #conv4_4
+            newlayer(64, 384, 64, 1) #conv4_5
+            newlayer(64, 384, 64, 1) #conv4_6
+            newlayer(64, 384, 96, 2) #conv4_7
+            newlayer(96, 576, 96, 1) #conv5_1
+            newlayer(96, 576, 96, 1) #conv5_2
+            newlayer(96, 576, 160, 2) #conv5_3
+            newlayer(160, 960, 160, 1) #conv6_1
+            newlayer(160, 960, 160, 1) #conv6_2
+            newlayer(160, 960, 320, 1) #conv6_3
+            nn.Conv2d(320, 1280, 1, 0, 0)
             nn.ReLU6(inplace = True)
-            nn.Conv2d(32, 32, 3, 1, 1, bias = False) #conv2_1/depthwise
-            nn.ReLU6(inplace = True)
-            nn.Conv2d(32, 16, 1, 1, 0, bias = False) #conv2_1/linear
-            nn.ReLU6(inplace = True) #need ammend
-            nn.Conv2d(16, 96, 1, 1, 0, bias = False) #conv2_2/expand
-            nn.ReLU6(inplace = True)
-            nn.Conv2d(96, 96, 3, 2, 1, bias = False) #conv2_2/depthwise
-            nn.ReLU6(inplace = True)
-            nn.Conv2d(96, 24, 1, 1, 0, bias = False) #conv2_2/linear
-            nn.ReLU6(inplace = True) #need ammend
-            nn.Conv2d(24, 144, 1, 1, 0, bias = False) #conv3_1/expand
-            nn.ReLU6(inplace = True)
-            nn.Conv2d(144, 144, 3, 1, 1, bias = False) #conv3_1/depthwise
-            nn.ReLU6(inplace = True)
-            nn.Conv2d(144, 24, 1, 1, 0, bias = False) #conv3_1/linear
-            nn.ReLU6(inplace = True) #need ammend
-            nn.Conv2d(24, 144, 1, 1, 0, bias = False) #conv4_1/expand
-            nn.ReLU6(inplace = True)
-            nn.Conv2d(144, 144, 3, 1, 1, bias = False) #conv4_1/depthwise
-            nn.ReLU6(inplace = True)
-            nn.Conv2d(144, 24, 1, 1, 0, bias = False) #conv4_1/linear
-            nn.ReLU6(inplace = True) #need ammend
-            nn.Conv2d(24, 1, 3, 2, 1, bias = False) #conv5_3/depthwise
-            nn.ReLU6(inplace = True)
-            nn.Conv2d(24, 1280, 1, 1, 0, bias = False)
-            
+            nn.AvgPool2d(7)
+
         )
         self.loss = RegionLoss([1,1.06357021727,1,2.65376815391],2)
         self.anchors = self.loss.anchors
